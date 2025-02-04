af5e8614ad8dda3585200b67777daec5725fe544
diydriller <dhrhd080@naver.com>
2025-02-04 16:37:26 +0900

    fix: payment service에서 포인트 사용 로직과 예약 확정 로직 분리

--- src/main/kotlin/io/hhplus/concert/application/payment/PaymentService.kt
+++ src/main/kotlin/io/hhplus/concert/application/payment/PaymentService.kt
@@ -1,83 +1,55 @@
 package io.hhplus.concert.application.payment
 
 import io.hhplus.concert.aop.lock.DistributedLock
-import io.hhplus.concert.domain.concert.ConcertReader
-import io.hhplus.concert.domain.concert.ConcertStore
+import io.hhplus.concert.application.point.PointCommand
+import io.hhplus.concert.application.point.PointService
+import io.hhplus.concert.application.reservation.ReservationService
 import io.hhplus.concert.domain.payment.Payment
 import io.hhplus.concert.domain.payment.PaymentStore
-import io.hhplus.concert.domain.point.PointReader
-import io.hhplus.concert.domain.point.PointStore
-import io.hhplus.concert.domain.reservation.ReservationDomainService
-import io.hhplus.concert.domain.reservation.ReservationReader
-import io.hhplus.concert.domain.reservation.ReservationStore
-import io.hhplus.concert.exception.ConflictException
-import io.hhplus.concert.exception.NotFoundException
-import io.hhplus.concert.response.BaseResponseStatus
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class PaymentService(
-    private val pointReader: PointReader,
-    private val pointStore: PointStore,
-    private val reservationReader: ReservationReader,
-    private val reservationStore: ReservationStore,
-    private val paymentStore: PaymentStore,
-    private val concertReader: ConcertReader,
-    private val concertStore: ConcertStore,
-    private val reservationDomainService: ReservationDomainService
+    private val reservationService: ReservationService,
+    private val pointService: PointService,
+    private val paymentStore: PaymentStore
 ) {
     @Transactional
-    fun pay(command: PaymentCommand) {
-        val userPoint = pointReader.findPointForUpdate(command.userId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_POINT)
-        val reservation = reservationReader.findReservationForUpdate(command.reservationId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_RESERVATION)
-        val seat = concertReader.findSeatForUpdate(reservation.seatId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_SEAT)
-        val concertSchedule = concertReader.findConcertScheduleForUpdate(reservation.concertScheduleId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_CONCERT_SCHEDULE)
+    fun payWithPessimisticLock(command: PaymentCommand) {
+        val reservedReservation = reservationService.completeReservationWithPessimisticLock(command.reservationId)
 
-        reservationDomainService.complete(seat, reservation, concertSchedule)
+        pointService.usePoint(
+            PointCommand.UsePoint(
+                userId = command.userId,
+                point = reservedReservation.price
+            )
+        )
 
-        userPoint.spend(reservation.price)
         val payment = Payment(
             userId = command.userId,
             reservationId = command.reservationId,
-            totalPrice = reservation.price
+            totalPrice = reservedReservation.price
         )
-
-        concertStore.saveConcertSchedule(concertSchedule)
-        concertStore.saveSeat(seat)
         paymentStore.savePayment(payment)
-        pointStore.savePoint(userPoint)
-        reservationStore.saveReservation(reservation)
     }
 
     @DistributedLock(key = "payment")
-    fun payWithRedissonLock(command: PaymentCommand) {
-        val userPoint = pointReader.findPoint(command.userId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_POINT)
-        val reservation = reservationReader.findReservation(command.reservationId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_RESERVATION)
-        val seat = concertReader.findSeat(reservation.seatId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_SEAT)
-        val concertSchedule = concertReader.findConcertSchedule(reservation.concertScheduleId)
-            ?: throw NotFoundException(BaseResponseStatus.NOT_FOUND_CONCERT_SCHEDULE)
+    fun payWithDistributedLock(command: PaymentCommand) {
+        val reservedReservation = reservationService.completeReservation(command.reservationId)
 
-        reservationDomainService.complete(seat, reservation, concertSchedule)
+        pointService.usePoint(
+            PointCommand.UsePoint(
+                userId = command.userId,
+                point = reservedReservation.price
+            )
+        )
 
-        userPoint.spend(reservation.price)
         val payment = Payment(
             userId = command.userId,
             reservationId = command.reservationId,
-            totalPrice = reservation.price
+            totalPrice = reservedReservation.price
         )
-
-        concertStore.saveConcertSchedule(concertSchedule)
-        concertStore.saveSeat(seat)
         paymentStore.savePayment(payment)
-        pointStore.savePoint(userPoint)
-        reservationStore.saveReservation(reservation)
     }
 }
