5e300c9890dc333ae29c7a98930e1fcedca593d7
diydriller <dhrhd080@naver.com>
2025-02-07 02:31:14 +0900

    test: redis를 사용한 대기열 테스트

--- src/test/kotlin/io/hhplus/concert/application/queue/QueueTokenSchedulerIntegrationTest.kt
+++ src/test/kotlin/io/hhplus/concert/application/queue/QueueTokenSchedulerIntegrationTest.kt
@@ -1,14 +1,18 @@
 package io.hhplus.concert.application.queue
 
-import com.github.f4b6a3.tsid.TsidCreator
 import io.hhplus.concert.config.BaseIntegrationTest
 import io.hhplus.concert.domain.queue.QueueToken
+import io.hhplus.concert.domain.queue.RedisQueueToken
 import io.hhplus.concert.infrastructure.queue.QueueTokenRepository
+import io.hhplus.concert.util.StringUtil.Companion.ACTIVE_QUEUE_KEY_PREFIX
+import io.hhplus.concert.util.StringUtil.Companion.WAITING_QUEUE_KEY
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 @SpringBootTest
 class QueueTokenSchedulerIntegrationTest : BaseIntegrationTest() {
@@ -18,15 +22,15 @@ class QueueTokenSchedulerIntegrationTest : BaseIntegrationTest() {
     @Autowired
     private lateinit var queueTokenRepository: QueueTokenRepository
 
+    @Autowired
+    private lateinit var redisTemplate: RedisTemplate<String, String>
+
     @Test
-    fun `토큰 활성화 함수 호출시 10개씩 활성화된다`() {
+    fun `토큰 활성화시 10개씩 활성화된다 (rdb 사용)`() {
         // given
         val queueTokenIdList: MutableList<String> = mutableListOf()
         repeat(30) {
-            val queueToken = QueueToken(
-                userId = TsidCreator.getTsid().toString(),
-                expiration = LocalDateTime.now().plusMinutes(10)
-            )
+            val queueToken = QueueToken()
             val savedToken = queueTokenRepository.save(queueToken)
             queueTokenIdList.add(savedToken.id)
         }
@@ -39,6 +43,31 @@ class QueueTokenSchedulerIntegrationTest : BaseIntegrationTest() {
         queueTokenIdList.forEach { queueTokenId ->
             val savedToken = queueTokenRepository.findQueueTokenById(queueTokenId)
             if (savedToken?.status == QueueToken.Status.ACTIVE) {
+                activeTokenCount++
+            }
+        }
+        assertEquals(10, activeTokenCount)
+    }
+
+    @Test
+    fun `토큰 활성화시 10개씩 활성화된다 (redis 사용)`() {
+        // given
+        val queueTokenIdList: MutableList<String> = mutableListOf()
+        repeat(30) {
+            val queueToken = RedisQueueToken()
+            val score = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond().toDouble()
+            redisTemplate.opsForZSet().add(WAITING_QUEUE_KEY, queueToken.id, score)
+            queueTokenIdList.add(queueToken.id)
+        }
+
+        // when
+        queueTokenScheduler.activateRedisQueueToken()
+
+        // then
+        var activeTokenCount = 0
+        queueTokenIdList.forEach { queueTokenId ->
+            val key = ACTIVE_QUEUE_KEY_PREFIX + queueTokenId
+            redisTemplate.opsForHash<String, String>().entries(key)["token"]?.run {
                 activeTokenCount++
             }
         }
